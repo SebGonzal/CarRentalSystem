e26df8cf3ebb79535c7834324f80b465fac212ca
ahall1315 <94080373+ahall1315@users.noreply.github.com>
2021-11-12 20:33:58 -0500

    Startup / Logout Controllers

---
+++ CarRentalSystem/Controllers/StartupControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CarRentalSystem.Controllers;
+using CarRentalSystem.Entity;
+using System.Diagnostics;
+
+namespace CarRentalSystem.Controllers
+{
+    class StartupControl
+    {
+        public static void Initiate()
+        {
+            // Initializes the database
+            DBConnector.InitializeDB();
+
+            // Opens the Login Form
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+
+        }
+    }
+}
