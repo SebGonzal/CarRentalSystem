dd0d46bf2d0569f6e3f35515b06d33965656ae9c
BMcclammy99 <94157593+BMcclammy99@users.noreply.github.com>
2021-11-18 19:15:41 -0500

    Add files via upload

---
+++ CarRentalSystem/Boundary/ViewAvailability.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace CarRentalSystem.Boundary
+{
+    partial class ViewAvailability
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Startdate = new System.Windows.Forms.DateTimePicker();
+            this.Enddate = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Message = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(661, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Logout";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(661, 387);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "View";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Startdate
+            // 
+            this.Startdate.Location = new System.Drawing.Point(64, 94);
+            this.Startdate.MinDate = new System.DateTime(2021, 11, 18, 0, 0, 0, 0);
+            this.Startdate.Name = "Startdate";
+            this.Startdate.Size = new System.Drawing.Size(262, 22);
+            this.Startdate.TabIndex = 2;
+            // 
+            // Enddate
+            // 
+            this.Enddate.Location = new System.Drawing.Point(384, 94);
+            this.Enddate.MinDate = new System.DateTime(2021, 11, 18, 0, 0, 0, 0);
+            this.Enddate.Name = "Enddate";
+            this.Enddate.Size = new System.Drawing.Size(263, 22);
+            this.Enddate.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(64, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 15);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Car Rental System";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(64, 178);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(262, 132);
+            this.listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(384, 178);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(263, 132);
+            this.listBox2.TabIndex = 6;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(307, 178);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(19, 132);
+            this.vScrollBar1.TabIndex = 7;
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.Location = new System.Drawing.Point(625, 178);
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(21, 132);
+            this.vScrollBar2.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(64, 122);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 50);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(385, 122);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(262, 50);
+            this.textBox3.TabIndex = 10;
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(329, 53);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(0, 17);
+            this.Message.TabIndex = 11;
+            // 
+            // ViewAvailability
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.vScrollBar2);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Enddate);
+            this.Controls.Add(this.Startdate);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "ViewAvailability";
+            this.Text = "ViewAvailability";
+            this.Load += new System.EventHandler(this.ViewAvailability_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker Startdate;
+        private System.Windows.Forms.DateTimePicker Enddate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label Message;
+    }
+}
---
+++ CarRentalSystem/Boundary/ViewAvailability.cs
@@ -0,0 +1,65 @@
+﻿using CarRentalSystem.Controllers;
+using CarRentalSystem.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRentalSystem.Boundary
+{
+
+    public partial class ViewAvailability : Form
+    {
+        public static ViewAvailability instance;
+        public ViewAvailability()
+        {
+            InitializeComponent();
+            instance = this;
+        }
+        public void Display()
+        {
+            ViewAvailability Vform = new ViewAvailability();
+            string startString = Startdate.Value.Year.ToString() + Startdate.Value.Month.ToString() + Startdate.Value.ToString("dd");
+            int startDate = int.Parse(startString);
+            string endString = Enddate.Value.Year.ToString() + Enddate.Value.Month.ToString() + Enddate.Value.ToString("dd");
+            int endDate = int.Parse(endString);
+            if (startDate > endDate) // validation to ensure user can't choose endDate earlier than startDate
+            {
+                Message.Text = "Invalid date entry.";
+            }
+            else
+            {
+                List<Vehicle> list = AvailControl.checkAvail(startDate, endDate);
+                {
+                    // dateTimePickers are disabled so user can't change dates after reaching "available" state,
+                    // can be reset by X'ing out window and reopening
+
+                    /*dateTimePicker_Start.Enabled = false;
+                    dateTimePicker_End.Enabled = false;
+                    btn_reserve.Enabled = true;
+                }
+
+                if (ReserveController.CheckReservation(ReserveController.selectedVehicle.GetVid(), startDate, endDate) == false)
+                {
+                    // reserve button will not be enabled until user inputs available dates
+                    lbl_message.Text = "Unavailable";
+                    btn_reserve.Enabled = false;*/
+                }
+            }
+        }
+    
+    public void Submit()
+    {
+
+    }
+    private void ViewAvailability_Load(object sender, EventArgs e)
+    {
+
+    }
+}
+}
